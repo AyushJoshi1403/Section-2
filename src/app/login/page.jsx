9814a436ba42785e4a9db76986a565e104fa7034
AyushJoshi1403 <ayush.aj1403@gmail.com>
2024-10-08 17:18:07 +0530

    react

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -4,6 +4,9 @@ const Login = () => {
   return (
     <div>
         <h2 className="text-5xl text-center">Login Page</h2>
+
+        <button className="submit-btn">Login button</button>
+
     </div>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,13 @@ const Home = () => {
   return (
     <div>
         <h1 className="text-4xl font-bold text-center" >Hello React</h1>
+        <button style={{ backgroundColor : 'black', color: 'white', padding: 10 }} >
+          Home button
+        </button>
+        <br />
+        <input type="text" />
+
+        <button className="submit-btn">Home button</button>
     </div>
   )
 }
