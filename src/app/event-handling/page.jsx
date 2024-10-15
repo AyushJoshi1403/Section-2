f01931f5c2a46d09e07a054555c2b5e97f378520
AyushJoshi1403 <ayush.aj1403@gmail.com>
2024-10-15 16:42:10 +0530

    event handling

--- src/app/event-handling/page.jsx
+++ src/app/event-handling/page.jsx
@@ -2,35 +2,52 @@
 import React from 'react'
 
 const EventHandling = () => {
-  return (
-    <div className="w-[80%] mx-auto border border-red-500" 
-        onMouseMove={(e) => {console.log(e.clientX, e.clientY); }}
-    >
 
-        <h1 className="text-center my-5 font-bold text-3xl">Event Handling</h1>
+    const previewImage = (e) => {
+        const file = e.target.files[0];
 
-        <button className="bg-black text-white p-3"
-            onClick={ () => { alert('button was clicked') } }
-        >Click Event</button>
+        const reader = new FileReader();
+        
+        reader.onload = (data) => {
+            const img = document.createElement('img');
+            img.src = data.target.result;
+            document.body.appendChild(img);
+        }
 
-        <input type="text" 
-            onChange={(e) => { console.log(e.target.value); }}
-            className="border rounded p-3 mt-4 w-full" />
+        reader.readAsDataURL(file);
+    }
 
-        <input type="color"
-            onChange={(e) => { document.body.style.backgroundColor = e.target.value; }}
-            className="mt-5" />
+    return (
+        <div className="w-[80%] mx-auto border border-red-500"
+            onMouseMove={(e) => { }}
+        >
 
-        <input type="file"
-            onChange={(e) => { console.log(e.target.files);}}
-            multiple
-        className="block mt-5" />
+            <h1 className="text-center my-5 font-bold text-3xl">Event Handling</h1>
 
-        <input type="text" onKeyDown={(e) => { console.log(e.code);}}
-            className="border rounded p-3 mt-4 w-full" />
+            <button className="bg-black text-white p-3"
+                onClick={() => { alert('button was clicked') }}
+            >Click Event</button>
 
-    </div>
-  )
+            <input type="text"
+                onChange={(e) => { console.log(e.target.value); }}
+                className="border rounded p-3 mt-4 w-full" />
+
+            <input type="color"
+                onChange={(e) => { document.body.style.backgroundColor = e.target.value; }}
+                className="mt-5" />
+
+            <input type="file"
+                onChange={(e) => { console.log(e.target.files); }}
+                multiple
+                className="block mt-5" />
+
+            <input type="text" onKeyDown={(e) => { console.log(e.code); }}
+                className="border rounded p-3 mt-4 w-full" />
+
+            <input onChange={previewImage} type="file" className='block mt-5' />
+
+        </div>
+    )
 }
 
 export default EventHandling;
