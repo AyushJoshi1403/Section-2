27ad4aeaf9fc4e7fe703a1c48a284fd1cf41d175
AyushJoshi1403 <ayush.aj1403@gmail.com>
2024-10-17 17:18:31 +0530

    react

--- src/app/todolist/page.jsx
+++ src/app/todolist/page.jsx
@@ -5,13 +5,35 @@ const TodoList = () => {
 
     const [count, setCount] = useState(0);
 
+    const [taskList, setTaskList] = useState([
+        { text: 'Doodh lao', completed: false },
+        { text: 'chai banao', completed: false },
+        { text: 'so jao', completed: false },
+    ]);
+
     const addNewTask = (e) => {
         if (e.code === 'Enter') {
             console.log(e.target.value);
 
+            const newTask = { text : e.target.value, completed: false };
+
+            setTaskList( [ ...taskList, newTask ] );
+
             e.target.value = '';
         }
     }
+
+    const deleteTask = (index) => {
+        const temp = taskList;
+        temp.splice(index, 1);
+        setTaskList([...temp]);
+    }
+
+    const toggleComplete = (index) => {
+        const temp = taskList;
+        temp[index].completed = !temp[index].completed;
+        setTaskList([...temp]);
+    } 
 
     return (
         <div>
@@ -20,7 +42,7 @@ const TodoList = () => {
             <div className='max-w-[80%] mx-auto'>
                 <p className='text-3xl'>{count}</p>
                 <button className='border p-3'
-                    onClick={() => { setCount(count +1); console.log(count); }} >Add Count</button>
+                    onClick={() => { setCount(count + 1); console.log(count); }} >Add Count</button>
                 <div className='rounded border border-gray-300 shadow'>
                     <div className='p-5'>
                         <input
@@ -30,7 +52,31 @@ const TodoList = () => {
                         />
                     </div>
                     <div className='border-t border-gray-300 p-5'>
+                        {
+                            taskList.map((task, index) => {
+                                return (
+                                    <div className='border p-3 rounded mb-4'>
+                                        {
+                                            task.completed ?
+                                            <p className='bg-green-500 text-white px-3 rounded-full w-fit'>Completed</p> :
+                                            <p className='bg-yellow-500 text-white px-3 rounded-full w-fit'>Pending</p>
+                                        }
+                                        <h2>{task.text}</h2>
 
+                                        <div>
+                                            <button
+                                                onClick={ () => {deleteTask(index) } }
+                                                className='bg-red-500 rounded-full py-1 px-3 text-white'
+                                            >Delete</button>
+                                            <button
+                                                onClick={() => { toggleComplete(index) }}
+                                                className={`bg-${task.completed ? 'green' : 'blue'}-500 rounded-full py-1 px-3 text-white`}
+                                            >{task.completed ? 'Undo' : 'Complete Task'}</button>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
                     </div>
                 </div>
             </div>
